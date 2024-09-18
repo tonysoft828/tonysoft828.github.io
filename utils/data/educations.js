9cb4f3415bb0dad199ced83db3886c923b703b40
tonysoft828 <ther828tony@gmail.com>
2024-09-19 00:40:44 +0900

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,6 +3,6 @@ export const educations = [
     id: 1,
     title: "Bachelor Degree",
     duration: "2010 - 2014",
-    institution: "Beijing University of Posts and Telecommunications",
+    institution: "Cornell University",
   },
 ]
